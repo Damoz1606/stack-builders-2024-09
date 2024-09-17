286d8eb9715018694bdfec06db7848ead78e77f1
Damoz1606 <panchitodmun@gmail.com>
2024-09-17 18:36:38 -0500

    :sparkles: Option changer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
 import { ModeToggle } from "@/components/mode-toggle";
+import { buttonVariants } from "@/components/ui/button";
+import clsx from "clsx";
+import Link from "next/link";
 
-export default function Home() {
+interface HomeProps {
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export default function Home({ searchParams }: HomeProps) {
+
+  const state = searchParams.state ? searchParams.state : 'first';
+
   return (
     <main className="relative w-screen h-screen">
-      <section className="absolute top-0 left-0 w-screen px-2 py-4">
+      <section className="w-full px-2 py-4">
         <ModeToggle />
       </section>
-      <section className="w-full h-full">
-
+      <section className="w-full px-4">
+        <div className="flex justify-between gap-2 p-2 rounded-md dark:bg-slate-100/10 bg-slate-200/40">
+          <Link href='?state=first' className={clsx('w-full', buttonVariants({ variant: state === 'first' ? 'default' : 'ghost' }))}>First option</Link>
+          <Link href='?state=second' className={clsx('w-full', buttonVariants({ variant: state === 'second' ? 'default' : 'ghost' }))}>Second option</Link>
+        </div>
       </section>
     </main>
   );
